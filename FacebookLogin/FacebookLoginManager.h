e7173c5b5c9185d7bf515956488b2891dfcd0b27
Lucas <lucas@agtlucas.com>
2015-07-30 20:52:49 -0300

    Adding FacebookLoginManager.h

---
+++ FacebookLogin/FacebookLoginManager.h
@@ -0,0 +1,13 @@
+//
+//  FacebookLoginManager.h
+//  FacebookLogin
+//
+//  Created by Lucas da Silva on 7/30/15.
+//  Copyright © 2015 Facebook. All rights reserved.
+//
+
+#import "RCTBridgeModule.h"
+
+@interface FacebookLoginManager : NSObject <RCTBridgeModule>
+
+@end
