53eeff8cf6633d3b91d8503b87f9487efa601a5a
Lucas <lucas@agtlucas.com>
2015-07-30 19:56:54 -0300

    Update index for FacebookLogin

--- FacebookLogin/index.ios.js
+++ FacebookLogin/index.ios.js
@@ -16,18 +16,33 @@ var {
 var FacebookLoginManager = require('NativeModules').FacebookLoginManager;
 
 var FacebookLogin = React.createClass({
+
+  getInitialState() {
+    return {
+      result: '...'
+    }
+  },
+
+  login() {
+    FacebookLoginManager.newSession((error, info) => {
+      if (error) {
+        this.setState({result: error});
+      } else {
+        this.setState({result: info});
+      }
+    });
+  },
+
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
+        <TouchableHighlight onPress={this.login}>
+          <Text style="styles.welcome">
+            Facebook Login
+          </Text>
+        </TouchableHighlight>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          {this.state.result}
         </Text>
       </View>
     );
