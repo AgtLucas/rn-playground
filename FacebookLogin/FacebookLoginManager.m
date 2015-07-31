63e3d2b445ebe85b7ddfb35a695736674e162ac8
Lucas <lucas@agtlucas.com>
2015-07-30 21:06:40 -0300

    Updates.

---
+++ FacebookLogin/FacebookLoginManager.m
@@ -0,0 +1,40 @@
+//
+//  FacebookLoginManager.m
+//  FacebookLogin
+//
+//  Created by Lucas da Silva on 7/30/15.
+//  Copyright © 2015 Facebook. All rights reserved.
+//
+
+#import "FacebookLoginManager.h"
+#import "FBSDKCoreKit/FBSDKCoreKit.h"
+#import "FBSDKLoginKit/FBSDKLoginKit.h"
+
+
+@implementation FacebookLoginManager
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(newSession:(RCTResponseSenderBlock)callback) {
+
+  FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+  
+  [login logInWithReadPermissions:@[@"public_profile", @"email"] handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+    
+    if (error) {
+      callback(@[@"Error", [NSNull null]]);
+    } else if (result.isCancelled) {
+      callback(@[@"Cancelad", [NSNull null]]);
+    } else {
+      FBSDKAccessToken *token = result.token;
+      NSString *tokenString = token.tokenString;
+      NSString *userId = token.userID;
+      NSDictionary *credentials = @{ @"token" : tokenString, @"userId" : userId };
+      callback(@[[NSNull null], credentials]);
+    }
+    
+  }];
+  
+};
+
+@end
