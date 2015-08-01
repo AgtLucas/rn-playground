d5288e042b3445acb056382bf8a26d8cecbebcf1
Lucas <lucas@agtlucas.com>
2015-08-01 15:38:36 -0300

    Rename project name

--- ScrollableTabView/index.ios.js
+++ ScrollableTabView/index.ios.js
@@ -10,22 +10,19 @@ var {
   StyleSheet,
   Text,
   View,
+  ScrollView,
+  Dimensions
 } = React;
 
-var ScrollableTabView = React.createClass({
+var ScrollableTabView = require('react-native-scrollable-tab-view');
+var FacebookTabBar = require('./FacebookTabBar');
+var deviceWidth = Dimensions.get('window').width;
+
+var App = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
       </View>
     );
   }
@@ -34,20 +31,25 @@ var ScrollableTabView = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: 30,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  tabView: {
+    width: deviceWidth,
+    padding: 10,
+    backgroundColor: 'rgba(0,0,0,0.1)',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  card: {
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+    borderColor: 'rgba(0,0,0,0.1)',
+    margin: 5,
+    height: 150,
+    padding: 15,
+    shadowColor: '#CCC',
+    shadowOffset: {width: 2, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
   },
 });
 
-AppRegistry.registerComponent('ScrollableTabView', () => ScrollableTabView);
+AppRegistry.registerComponent('App', () => App);
